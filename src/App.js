f2e6f9f1455da983e30c67dce31c840525bdf054
Sanithi Shavindi <sandayanasanithi@gmail.com>
2024-04-21 20:24:59 +0530

    1111

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import { Routes, Route, Link,Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Login } from './Components/LoginSignup/Login';
 import { Signup } from './Components/LoginSignup/Signup';
@@ -14,12 +14,12 @@ function App() {
     <div className='App'>
 
         <div>
-          <nav>
+          {/* <nav>
             <ul>
               <li><Link to="/">Signup</Link></li>
               <li><Link to="/Login">Login</Link></li>
             </ul>
-          </nav>
+          </nav> */}
           <Routes>
             <Route path="/" element={<Signup/>}/>
             <Route path="/Login" element={<Login/>}/>
