5eba215736743878738c00e63f4fc13ec55eb2c9
Sanithi Shavindi <sandayanasanithi@gmail.com>
2024-06-25 22:51:45 +0530

    1111

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import Doctor from '../Pages/Doctors';
+import Home from '../Pages/Home';
+import Resources from '../Pages/Resources';
+import Specialization from '../Pages/Specialization';
 
-function Layout() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Layout = () => {
+  
+  return(
+    <Router>
+      <div>
+        <Navbar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
 
 export default Layout
