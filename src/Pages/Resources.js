d2d882b051a78f2359ded174a7f3db4db916b32b
Sanithi Shavindi <sandayanasanithi@gmail.com>
2024-07-03 00:09:04 +0530

    1111

--- src/Pages/Resources.js
+++ src/Pages/Resources.js
@@ -9,8 +9,20 @@ function Resources() {
         <div className='Resources'>
         <h1 className='text-3xl' >Resources</h1>
         </div>
-        <div>
-          
+        <div className='my-10'>
+        1.	Defibrillator<br></br>
+2.	Suction Machine<br></br>
+3.	Emergency Stretchers<br></br>
+4.	Stethoscope<br></br>
+5.	Cervical Collar<br></br>
+6.	intubation cannula<br></br>
+7.	Ventilators<br></br>
+8.	ECG Machine<br></br>
+9.	Surgical Masks, Surgical Gloves<br></br>
+10.	Patient Monitors<br></br>
+11.	syringe pump<br></br>
+12.	Cardiac monitor 
+
         </div>
     </div>
   )
