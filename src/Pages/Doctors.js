17d0b58b8c069cb92a52181dd7b9a042852fe654
Sanithi Shavindi <sandayanasanithi@gmail.com>
2024-07-02 12:40:36 +0530

    1111

--- src/Pages/Doctors.js
+++ src/Pages/Doctors.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import doc from '../Components/Assests/doc.png';
+
 
 function Doctor() {
   return (
@@ -7,7 +9,14 @@ function Doctor() {
     <div>
          
         <div className='Doctor'>
-        <h1>doctor</h1>
+        <h1 className='font-normal hover:font-bold text-xl'>Doctor Details</h1>
+        </div>
+
+        <div className='mx-80 size-24'>
+          <img src={doc} alt=""/>
+          <div className="bg-white border border-gray-300 rounded-lg p-4 m-4 shadow-md text-center w-64  ">
+          <p className="mb-4 text-gray-700">Dengue (break-bone fever) is a viral infection that spreads from mosquitoes to people.</p>
+          </div>
         </div>
     </div>
   )
