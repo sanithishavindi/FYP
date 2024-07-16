b4b61304eee2f27582bffe5ba6e8cb9970d6d20d
Sanithi Shavindi <sandayanasanithi@gmail.com>
2024-07-16 12:27:01 +0530

    1111

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDstWGn7NOz_nH3yccMFsbgXNj4dEplSkU",
+  authDomain: "final-70629.firebaseapp.com",
+  projectId: "final-70629",
+  storageBucket: "final-70629.appspot.com",
+  messagingSenderId: "399540071097",
+  appId: "1:399540071097:web:879b222b1434d94b7b0968"
+};
+
+
+const app = initializeApp(firebaseConfig);
